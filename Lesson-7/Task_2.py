8cd6729ff22faf00b83ccae80204f3475e737a70
Ka4mar <Berserk_ka4mar@mail.ru>
2021-04-04 19:12:57 +0300

    Lesson-7

---
+++ Lesson-7/Task_2.py
@@ -0,0 +1,112 @@
+"""
+2. Отсортируйте по возрастанию методом слияния одномерный вещественный массив,
+заданный случайными числами на промежутке [0; 50). Выведите на экран исходный
+и отсортированный массивы.
+Хотя в примерах к уроку уже есть вариант реализации слияния,
+попробуйте предложить другой (придумать или найти)
+И попытаться сделать замеры на массивах разной длины: 10, 100, 1000, ...
+Пример:
+Введите число элементов: 5
+Исходный - [46.11436617832828, 41.62921998361278, 18.45859540989644, 12.128870723745806, 8.025098788570562]
+Отсортированный - [8.025098788570562, 12.128870723745806, 18.45859540989644, 41.62921998361278, 46.11436617832828]
+"""
+
+import timeit
+import random
+
+
+def merge(left_list, right_list):
+    sorted_list = []
+    left_list_index = right_list_index = 0
+
+    # Длина списков часто используется, поэтому создадим переменные для удобства
+    left_list_length, right_list_length = len(left_list), len(right_list)
+
+    for _ in range(left_list_length + right_list_length):
+        if left_list_index < left_list_length and right_list_index < right_list_length:
+            # Сравниваем первые элементы в начале каждого списка
+            # Если первый элемент левого подсписка меньше, добавляем его
+            # в отсортированный массив
+            if left_list[left_list_index] <= right_list[right_list_index]:
+                sorted_list.append(left_list[left_list_index])
+                left_list_index += 1
+            # Если первый элемент правого подсписка меньше, добавляем его
+            # в отсортированный массив
+            else:
+                sorted_list.append(right_list[right_list_index])
+                right_list_index += 1
+
+        # Если достигнут конец левого списка, элементы правого списка
+        # добавляем в конец результирующего списка
+        elif left_list_index == left_list_length:
+            sorted_list.append(right_list[right_list_index])
+            right_list_index += 1
+        # Если достигнут конец правого списка, элементы левого списка
+        # добавляем в отсортированный массив
+        elif right_list_index == right_list_length:
+            sorted_list.append(left_list[left_list_index])
+            left_list_index += 1
+
+    return sorted_list
+
+
+def merge_sort(nums):
+    # Возвращаем список, если он состоит из одного элемента
+    if len(nums) <= 1:
+        return nums
+
+    # Для того чтобы найти середину списка, используем деление без остатка
+    # Индексы должны быть integer
+    mid = len(nums) // 2
+
+    # Сортируем и объединяем подсписки
+    left_list = merge_sort(nums[:mid])
+    right_list = merge_sort(nums[mid:])
+
+    # Объединяем отсортированные списки в результирующий
+    return merge(left_list, right_list)
+
+# Проверяем, что оно работает
+
+
+random_list_of_nums = [random.randint(0, 50) for _ in range(10)]
+print(random_list_of_nums[0:5])
+random_list_of_nums = merge_sort(random_list_of_nums)
+
+
+print(
+    timeit.timeit(
+        "merge_sort(random_list_of_nums[:])",
+        globals=globals(),
+        number=1000))
+
+print(merge_sort(random_list_of_nums)[0:5])
+
+
+random_list_of_nums2 = [random.randint(0, 50) for _ in range(100)]
+
+
+print(
+    timeit.timeit(
+        "merge_sort(random_list_of_nums2[:])",
+        globals=globals(),
+        number=1000))
+
+
+random_list_of_nums3 = [random.randint(0, 50) for _ in range(1000)]
+
+
+print(
+    timeit.timeit(
+        "merge_sort(random_list_of_nums3[:])",
+        globals=globals(),
+        number=1000))
+
+
+"""
+[4, 11, 33, 16, 17] - исходный массив
+0.020000864  - время с 10 числами
+[4, 9, 11, 16, 16] - отсортированный 
+0.305748703 - время с 100 элементов 
+4.012189010999999 - время с 1000 элементов 
+"""
